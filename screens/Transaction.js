d2c16c1f1d52a2bf0933c4d8937f1dfbcce707f2
karla-byjus <102175079+karla-byjus@users.noreply.github.com>
2022-03-23 16:04:18 -0600

    Traducción 1.1

--- screens/Transaction.js
+++ screens/Transaction.js
@@ -38,8 +38,8 @@ export default class TransactionScreen extends Component {
     const { status } = await Permissions.askAsync(Permissions.CAMERA);
 
     this.setState({
-      /*status === "granted" is true when user has granted permission
-          status === "granted" is false when user has not granted the permission
+      /*status === "granted" es verdadero cuando el usuario da su permiso.
+          status === "granted" es falso cuando el usuario no da su permiso.
         */
       hasCameraPermissions: status === "granted",
       domState: domState,
@@ -74,8 +74,8 @@ export default class TransactionScreen extends Component {
 
     if (!transactionType) {
       this.setState({ bookId: "", studentId: "" });
-      // For Android users only
-      // ToastAndroid.show("The book doesn't exist in the library database!", ToastAndroid.SHORT);
+      // Solo para usuarios de Android.
+      // ToastAndroid.show("El libro no existe en la base de datos de la biblioteca.", ToastAndroid.SHORT);
       Alert.alert("El libro no existe en la base de datos de la biblioteca.");
     } else if (transactionType === "issue") {
       var isEligible = await this.checkStudentEligibilityForBookIssue(
@@ -86,9 +86,9 @@ export default class TransactionScreen extends Component {
         var { bookName, studentName } = this.state;
         this.initiateBookIssue(bookId, studentId, bookName, studentName);
       }
-      // For Android users only
-      // ToastAndroid.show("Book issued to the student!", ToastAndroid.SHORT);
-      Alert.alert("Book issued to the student!");
+      // Solo para usuarios de Android.
+      // ToastAndroid.show("Se le prestó el libro al alumno.", ToastAndroid.SHORT);
+      Alert.alert("Se le prestó el libro al alumno.");
     } else {
       var isEligible = await this.checkStudentEligibilityForBookReturn(
         bookId,
@@ -99,9 +99,9 @@ export default class TransactionScreen extends Component {
         var { bookName, studentName } = this.state;
         this.initiateBookReturn(bookId, studentId, bookName, studentName);
       }
-      // For Android users only
-      // ToastAndroid.show("Book returned to the library!", ToastAndroid.SHORT);
-      Alert.alert("Book returned to the library!");
+      // Solo para usuarios de Android.
+      // ToastAndroid.show("Se devolvió el libro a la biblioteca.", ToastAndroid.SHORT);
+      Alert.alert("Se devolvió el libro a la biblioteca.");
     }
   };
 
@@ -144,8 +144,8 @@ export default class TransactionScreen extends Component {
       transactionType = false;
     } else {
       bookRef.docs.map(doc => {
-        //if the book is available then transaction type will be issue
-        // otherwise it will be return
+        //Si el libro está disponible, entonces el tipo de transacción se enviará.
+        // Si no, se devolverá.
         transactionType = doc.data().is_book_available ? "issue" : "return";
       });
     }
@@ -166,14 +166,14 @@ export default class TransactionScreen extends Component {
         studentId: ""
       });
       isStudentEligible = false;
-      Alert.alert("The student id doesn't exist in the database!");
+      Alert.alert("El id del alumno no existe en la base de datos.");
     } else {
       studentRef.docs.map(doc => {
         if (doc.data().number_of_books_issued < 2) {
           isStudentEligible = true;
         } else {
           isStudentEligible = false;
-          Alert.alert("The student has already issued 2 books!");
+          Alert.alert("Ya se le prestaron 2 libros al alumno.");
           this.setState({
             bookId: "",
             studentId: ""
@@ -198,7 +198,7 @@ export default class TransactionScreen extends Component {
         isStudentEligible = true;
       } else {
         isStudentEligible = false;
-        Alert.alert("The book wasn't issued by this student!");
+        Alert.alert("Este alumno no envió este libro.");
         this.setState({
           bookId: "",
           studentId: ""
@@ -209,7 +209,7 @@ export default class TransactionScreen extends Component {
   };
 
   initiateBookIssue = async (bookId, studentId, bookName, studentName) => {
-    //add a transaction
+    //Añade una transacción.
     db.collection("transactions").add({
       student_id: studentId,
       student_name: studentName,
@@ -218,20 +218,20 @@ export default class TransactionScreen extends Component {
       date: firebase.firestore.Timestamp.now().toDate(),
       transaction_type: "issue"
     });
-    //change book status
+    //Cambia el estatus del libro.
     db.collection("books")
       .doc(bookId)
       .update({
         is_book_available: false
       });
-    //change number  of issued books for student
+    //Cambia el número de libros prestados del alumno.
     db.collection("students")
       .doc(studentId)
       .update({
         number_of_books_issued: firebase.firestore.FieldValue.increment(1)
       });
 
-    // Updating local state
+    // Actualizando el estado local.
     this.setState({
       bookId: "",
       studentId: ""
@@ -239,7 +239,7 @@ export default class TransactionScreen extends Component {
   };
 
   initiateBookReturn = async (bookId, studentId, bookName, studentName) => {
-    //add a transaction
+    //Añade una transacción.
     db.collection("transactions").add({
       student_id: studentId,
       student_name: studentName,
@@ -248,20 +248,20 @@ export default class TransactionScreen extends Component {
       date: firebase.firestore.Timestamp.now().toDate(),
       transaction_type: "return"
     });
-    //change book status
+    //Cambia el estatus del libro.
     db.collection("books")
       .doc(bookId)
       .update({
         is_book_available: true
       });
-    //change number  of issued books for student
+    //Cambia el número de libros prestados del alumno.
     db.collection("students")
       .doc(studentId)
       .update({
         number_of_books_issued: firebase.firestore.FieldValue.increment(-1)
       });
 
-    // Updating local state
+    // Actualizando el estado local.
     this.setState({
       bookId: "",
       studentId: ""
@@ -298,7 +298,7 @@ export default class TransactionScreen extends Component {
                 style={styles.scanbutton}
                 onPress={() => this.getCameraPermissions("bookId")}
               >
-                <Text style={styles.scanbuttonText}>Scan</Text>
+                <Text style={styles.scanbuttonText}>Escanear</Text>
               </TouchableOpacity>
             </View>
             <View style={[styles.textinputContainer, { marginTop: 25 }]}>
@@ -313,14 +313,14 @@ export default class TransactionScreen extends Component {
                 style={styles.scanbutton}
                 onPress={() => this.getCameraPermissions("studentId")}
               >
-                <Text style={styles.scanbuttonText}>Scan</Text>
+                <Text style={styles.scanbuttonText}>Escanear</Text>
               </TouchableOpacity>
             </View>
             <TouchableOpacity
               style={[styles.button, { marginTop: 25 }]}
               onPress={this.handleTransaction}
             >
-              <Text style={styles.buttonText}>Submit</Text>
+              <Text style={styles.buttonText}>Enviar</Text>
             </TouchableOpacity>
           </View>
         </ImageBackground>
